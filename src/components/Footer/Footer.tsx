21a54b7e3d29b480eb0dcea95f11bf04e5029eef
milan9111 <milan9111@ukr.net>
2023-01-13 12:05:20 +0200

    added address at footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { IFooterContent } from "../../types/contents";
 
 interface footerProps {
   footerContent: IFooterContent;
+  contractAddress: string;
 }
 
-const Footer: React.FC<footerProps> = ({ footerContent }) => {
+const Footer: React.FC<footerProps> = ({ footerContent, contractAddress }) => {
   const footerLinks = footerContent.links.map((item) => {
     return (
       <li key={item.id} className="footer__link">
@@ -18,6 +19,16 @@ const Footer: React.FC<footerProps> = ({ footerContent }) => {
   return (
     <footer className="footer">
       <div className="footer__container">
+        <div className="footer__contract">
+          Smart Contact:{" "}
+          <a
+            href={`https://goerli.etherscan.io/address/${contractAddress}`}
+            rel="noreferrer"
+            target="_blank"
+          >
+            {contractAddress}
+          </a>
+        </div>
         <nav className="footer__menu">
           <ul>{footerLinks}</ul>
         </nav>
--- src/components/Footer/FooterContainer.tsx
+++ src/components/Footer/FooterContainer.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import './footer.scss';
+import "./footer.scss";
 import { footerContent } from "../../content/footerContent";
+import { contractAddress } from "../../web3/connectWallet";
 import Footer from "./Footer";
 
-
-
 const FooterContainer = () => {
-    return(
-        <Footer footerContent={footerContent}/>
-    )
-}
+  return (
+    <Footer footerContent={footerContent} contractAddress={contractAddress} />
+  );
+};
 
 export default FooterContainer;
