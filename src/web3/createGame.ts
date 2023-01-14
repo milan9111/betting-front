e838d67a2ecc0018301c080d3fa9b557d4ca0016
milan9111 <milan9111@ukr.net>
2023-01-14 21:50:10 +0200

    fixed create match in contract

--- src/web3/createGame.ts
+++ src/web3/createGame.ts
@@ -6,12 +6,15 @@ export const createGameInContract = async ({
   contract,
   userAccount,
 }: ICreateGamePayload) => {
-
   contract.once("MatchCreated", (index) => {
     const createGame = async () => {
-    const result = await MatchesApi.createGame(item, userAccount, Number(index));
-    console.log(result); // alert
-    } 
+      const result = await MatchesApi.createGame(
+        item,
+        userAccount,
+        Number(index)
+      );
+      console.log(result); // alert
+    };
     createGame();
   });
 
@@ -21,24 +24,24 @@ export const createGameInContract = async ({
     item.odds_2 && Math.floor(Number(item.odds_2.toFixed(2)) * 100);
   const coefficientDraw =
     item.odds_x && Math.floor(Number(item.odds_x.toFixed(2)) * 100);
+
+  const year = +item.event_date.split("-")[0];
+  const month = +item.event_date.split("-")[1] - 1;
+  const day = +item.event_date.split("-")[2];
+  const hours = +item.event_time.split(":")[0];
+  const minutes = +item.event_time.split(":")[1];
+
   const startMatch =
-    new Date(`${item.event_date} ${item.event_time}`).getTime() / 1000 + 3600; //3600  GMT+2
+    new Date(year, month, day, hours, minutes, 0).getTime() / 1000 + 3600; //3600  GMT+2
   const endMatch = startMatch + 10800;
 
-  
-  alert(new Date(+item.event_date.split('-')[0], +item.event_date.split('-')[1] - 1, +item.event_date.split('-')[2], +item.event_time.split(':')[0], +item.event_time.split(':')[1], 0).getTime());
-  console.log(`${item.event_date} ${item.event_time}`);
-  
-  alert(startMatch);
-  alert(endMatch);
+  const tx = await contract.createMatch(
+    coefficientOne,
+    coefficientTwo,
+    coefficientDraw,
+    startMatch,
+    endMatch
+  );
 
-  // const tx = await contract.createMatch(
-  //   coefficientOne,
-  //   coefficientTwo,
-  //   coefficientDraw,
-  //   startMatch,
-  //   endMatch
-  // );
-
-  // return tx;
+  return tx;
 };
